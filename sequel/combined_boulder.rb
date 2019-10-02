7abe01dab52ed38312e2b7a4f12801cf4cbbcb7b
Tim Hatch <hatch.tim@gmail.com>
2019-10-02 12:42:17 +0100

    Test jsonb updates
    Test inserting data into a jsonb record

--- sequel/combined_boulder.rb
+++ sequel/combined_boulder.rb
@@ -23,6 +23,16 @@ end
 
 # Test output
 CombinedBoulderResults.route(wet_id: 1, grp_id: 5, route: 2)
-                      .each { |x| p x }
+#                      .each { |x| p x }
 
+# Create a JSONB Operation object for the "data" jsonb column in our table
+jsonb = Sequel.pg_jsonb_op(:result_jsonb)
 
+# dataset = @db[:results].where(wet_id: 1, route: 2, per_id: 5090)
+route = CombinedBoulderResults.where(wet_id: 1, route: 2, grp_id: 5)
+dataset = route[per_id: 5090]
+
+hash = { p1: { a: 3, b: 2, t: 0 }, p2: { a: 3, b: 4, t: nil } }
+dataset.update(result_jsonb: jsonb.concat(hash))
+
+# p dataset[:result_jsonb]
