ec00b682a7651d276f163e848e10f266222f25d7
Tim Hatch <hatch.tim@gmail.com>
2019-10-03 16:01:58 +0100

    Refactor ranking matrix assemblers
    Refactor the ranking calculations to use Vector addition and reduce() rather than each.with_index() enumerations

--- sequel/boulder_modus.rb
+++ sequel/boulder_modus.rb
@@ -1,85 +1,78 @@
 # Module  Perseus                 - The namespace for application code
 # Module  StandardBoulderModus    - IFSCBoulderModus methods
 
+require 'matrix'
 require 'ranker'
 
 module Perseus
   module StandardBoulderModus
     # private_class_method
 
-    # Simple helper to calculate tops/bonuses and the relevant number of attempts
-    # This function is not called if the relevant value is nil (doesn't exist), so
-    # internally we just need to check for it existing but having a value of zero
-    # @params
-    # - An array [x, y] holding the aggregated result and
-    # - A value passed in (may be zero)
-    # OPTIMIZE: Refactor this as a lambda?
-    def self.set_atts(array, value)
-      array[0] += 1 unless value.zero?
-      array[1] += value
-    end
-
-    # sig ( returns(Proc).returns(T::Array[Integer)) }
+    # ifsc1999 - ranking model prior to the 2018 sseason (T/TA/Z/ZA)
+    # sig ( returns(Proc.params(h: Hash).returns(T::Array[Integer])) }
     def self.ifsc1999
-      ->(h) { [h[:sort_values][0], -h[:sort_values][1], h[:sort_values][2], -h[:sort_values][3]] }
+      ->(h) { [h[:result][0], -h[:result][1], h[:result][2], -h[:result][3]] }
     end
 
-    # sig ( returns(Proc).returns(T::Array[Integer)) }
+    # ifsc2018 - ranking model for the 2018 sseason onward (T/Z/TA/ZA)
+    # sig ( returns(Proc.params(h: Hash).returns(T::Array[Integer])) }
     def self.ifsc2018
-      ->(h) { [h[:sort_values][0], h[:sort_values][2], -h[:sort_values][1], -h[:sort_values][3]] }
+      ->(h) { [h[:result][0], h[:result][2], -h[:result][1], -h[:result][3]] }
+    end
+
+    # score? - returns 1 if the input value is non-zero/non-nil, 0 otherwise
+    # sig { params(value: T.nilable(Integer)).returns(Integer) }
+    def self.score(value)
+      value.to_i.zero? ? 0 : 1
+    end
+
+    # subarray - returns an array of the attempts to gain either a Zone or a Top made on each
+    # [of four] boulders, ordered best (fewest attempts) to worst
+    # sig { params(hash: Hash, key: String, n: Integer).returns(T::Array[Integer]) }
+    def self.subarray(hash, key, n = 4) # rubocop:disable Naming/UncommunicativeMethodParamName
+      arr = Array.new(n, 0)
+
+      hash.reduce(arr) do |a, (_k, v)|
+        a.push(v[key].to_i)
+      end.sort.slice(-n, n)
     end
 
     module_function
 
-    # sig { params(Hash).returns(Hash) }
+    # RANKING METHODS
+
+    # sig { params(results: T::Array[Hash]).returns(T::Array[Hash]) }
     def base_rank(results)
-      data = results.map { |x| x.merge(sort_values: sortvalues(x[:result_jsonb])) }
+      data = results.map { |x| x.merge(result: to_result_array(x[:result_jsonb])) }
 
       Ranker.rank(data, asc: true, strategy: :standard_competition, by: ifsc2018)
             .flat_map { |x| x.rankables.map { |r| r.merge(base_rank: x.rank) } }
     end
 
-    # sig { params(Hash).returns(Hash) }
+    # sig { params(results: T::Array[Hash]).returns(T::Array[Hash]) }
     def tie_break(results)
-      data  = results.map { |x| x.merge(tie_breaks: tiebreaks(x[:result_jsonb])) }
-      order = ->(h) { h[:tie_breaks] }
+      order = ->(h) { to_attempt_array(h[:result_jsonb]) }
 
-      Ranker.rank(data, asc: false, strategy: :standard_competition, by: order)
+      Ranker.rank(results, asc: false, strategy: :standard_competition, by: order)
             .flat_map { |x| x.rankables.map { |r| r.merge(tie_break: x.rank) } }
-      # .map { |x| x.tap { |r| r.delete(:tie_breaks) } }
     end
 
-    # Calculate the overall result for the competitor (i.e. 1t2 3b4), storing the result in
-    # an array.
-    # sig { params(result_jsonb: Hash).returns(Array[Integer]) }
-    # NOTE: <result_jsonb> is a hash containing the result, e.g.
-    #       { p1: { a: 1, b: 1, t:1 }, p2: { a: 3, b: 3 } }
-    def sortvalues(result_jsonb)
-      barr = [0, 0]
-      tarr = [0, 0]
+    # RESULT AGGREGATION METHODS
 
-      unless result_jsonb.nil?
-        result_jsonb.each do |_k, v|
-          set_atts(barr, v['b']) unless v['b'].nil?
-          set_atts(tarr, v['t']) unless v['t'].nil?
-        end
-      end
-      tarr + barr
+    # result_array - return an integer array corresponding to the result T/TA/Z/ZA
+    # sig { params(hash: Hash).returns(T::Array[Integer]) }
+    # `hash` has the form: { p1: { a: 1, b: 1, t:1 }, p2: { a: 3, b: 3 } }
+    def to_result_array(hash)
+      vec = Vector.zero(4)
+
+      hash.reduce(vec) do |a, (_k, v)|
+        a + Vector[score(v['t']), v['t'].to_i, score(v['b']), v['b'].to_i]
+      end.to_a
     end
 
-    # sig { params(result_jsonb: Hash).returns(Array[Integer]) }
-    # TODO: Modify to deal with an arbitrary number of blocs (here assumes max = 4)
-    def tiebreaks(result_jsonb)
-      barr = [0, 0, 0, 0]
-      tarr = [0, 0, 0, 0]
-
-      unless result_jsonb.nil?
-        result_jsonb.each.with_index do |(_k, v), i|
-          barr[i] = v['b'] || 0
-          tarr[i] = v['t'] || 0
-        end
-      end
-      tarr.sort + barr.sort
+    # sig { params(hash: Hash).returns(T::Array[Integer]) }
+    def to_attempt_array(hash)
+      subarray(hash, 't') + subarray(hash, 'b')
     end
   end
 end
