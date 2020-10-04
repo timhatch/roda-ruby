321c83be83eb83cbde4f740b1d2e5f171419972e
Tim Hatch <hatch.tim@gmail.com>
2020-10-04 16:38:35 +0100

    Use resolvers to structure the root query
    See https://graphql-ruby.org/fields/resolvers.html#nesting-resolvers-of-the-same-type

--- graphql/query.rb
+++ graphql/query.rb
@@ -4,29 +4,42 @@ require 'graphql'
 
 require_relative 'person_type'
 
+module Resolvers
+  class BaseResolver < GraphQL::Schema::Resolver
+  end
+end
+
+module Resolvers
+  class Result < Resolvers::BaseResolver
+    description 'Return an array of result data'
+    type [Types::Result], null: true
+
+    argument :id, Integer, required: true
+
+    def resolve(id:)
+      DataModels::Result.all.take(id).map(&:values)
+    end
+  end
+end
+
+module Resolvers
+  class Athlete < Resolvers::BaseResolver
+    description 'Return data for a specified athlete'
+    type Types::Athlete, null: true
+
+    argument :id, Integer, required: true
+
+    def resolve(id:)
+      DataModels::Person.where(per_id: id).first.values
+    end
+  end
+end
+
 module Types
   class QueryType < GraphQL::Schema::Object
     description 'The query root of the schema'
 
-    field :person, Types::Athlete, null: true do
-      description 'Return data for a specified athlete'
-
-      argument :id, Integer, required: true
-    end
-
-    field :result, [Types::Result], null: true do
-      description 'Return an array of result data'
-
-      argument :id, Integer, required: true
-    end
-
-    def person(id:)
-      DataModels::Person.where(per_id: id).first.values
-    end
-
-    def result(id:)
-      # p  DataModels::Result.first.values
-      DataModels::Result.all.take(id).map(&:values)
-    end
+    field :person, resolver: Resolvers::Athlete
+    field :result, resolver: Resolvers::Result
   end
 end
